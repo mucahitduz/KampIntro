0e45ef5e505b220cc40088cd0a8ff0773b596f56
codbr <codbr@DESKTOP-C02U9OA>
2022-02-28 16:05:52 +0300

    Add project files.

---
+++ SecondDayHomework6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SecondDayHomework6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Product product1 = new Product();
+            product1.ProductName = "Laptop";
+            product1.ProductPrice = 10000;
+            product1.ProductQuantity = "300";
+            product1.ProductDiscount = 15;
+
+            Product product2 = new Product();
+            product2.ProductName = "TV";
+            product2.ProductPrice = 4000;
+            product2.ProductQuantity = "500";
+            product2.ProductDiscount = 8;
+
+            Product product3 = new Product();
+            product3.ProductName = "Telefon";
+            product3.ProductPrice = 7000;
+            product3.ProductQuantity = "150";
+            product3.ProductDiscount = 30;
+                        
+            Product[] products = new Product[] { product1, product2, product3 };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.ProductName + " " + product.ProductPrice + " " + product.ProductQuantity + " " + product.ProductDiscount);
+            }
+
+        }                  
+
+    }
+    class Product
+    {
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+        public string ProductQuantity { get; set; }
+        public int ProductDiscount { get; set; }
+    }
+}
