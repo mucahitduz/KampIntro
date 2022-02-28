2e19a07674662cfdc83f87705eb295ddece64322
Mücahit DÜZ <codbreaker24@gmail.com>
2022-02-28 16:29:58 +0300

    C# 3.Gün

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+     class Musteri
+    {
+        public int MusteriId { get; set; }
+        public string MusteriAdiSoyadi { get; set; }
+        public string MusteriTcNo { get; set; }
+    }
+}
+
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriId = 1;
+            musteri1.MusteriAdiSoyadi = "Mücahit DÜZ";
+            musteri1.MusteriTcNo = "12345678912";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriId = 1;
+            musteri2.MusteriAdiSoyadi = "Mücahit YAMUK";
+            musteri2.MusteriTcNo = "98765432198";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+                        
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            Console.WriteLine("------------");
+
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+
+            Console.WriteLine("------------");
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteriler listelendi : " + musteri.MusteriAdiSoyadi);
+            }
+
+        }
+    }
+}
