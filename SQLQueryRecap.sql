fedbe7f16de2e7ec2248d78683df651627f54d16
Mücahit DÜZ <codbreaker24@gmail.com>
2022-03-02 17:06:13 +0300

    Sql Query Recap

---
+++ SQLQueryRecap.sql
@@ -0,0 +1,3 @@
+﻿Select Products.ProductName UrunAdi , SUM([Order Details].UnitPrice) ToplamKazanilanPara from Products inner join [Order Details]
+on Products.ProductID = [Order Details].ProductID inner join Orders
+on Orders.OrderID = [Order Details].OrderID Group by Products.ProductName order by ToplamKazanilanPara
